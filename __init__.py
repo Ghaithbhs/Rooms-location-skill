e5363370384763f7d37c926cc50ebe0c145e08f6
Ghaithbhs <ghaith_bhs@yahoo.fr>
2019-04-10 10:44:58 +0000

    first modifications

--- __init__.py
+++ __init__.py
@@ -5,10 +5,15 @@ class FocusDepartments(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
 
-    @intent_file_handler('departments.focus.intent')
+#    @intent_file_handler('departments.focus.intent')
+#    def handle_departments_focus(self, message):
+#        self.speak_dialog('departments.focus')
+    @intent_handler(IntentBuilder().require("question"))
     def handle_departments_focus(self, message):
-        self.speak_dialog('departments.focus')
-
+	if message.data["question"] == "where is the I T" :
+	    self.speak_dialog('departments.focus')
+	else if message.data["question"] == "where is the cafeteria" :
+	    self.speak_dialog('cafeteria.focus')
 
 def create_skill():
     return FocusDepartments()
